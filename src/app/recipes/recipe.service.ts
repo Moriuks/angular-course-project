7db3e16fb34ff8dab2bfe763803a310be47f90d5
Luis Mora <luismora371@gmail.com>
2021-01-21 21:19:58 -0600

    new services and ingredient added

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,3 +1,4 @@
+import { Ingredient } from './../shared/ingredient.model';
 import { EventEmitter } from '@angular/core';
 import { Recipe } from "./recipe.model";
 
@@ -5,8 +6,22 @@ export class RecipeService{
   recipeSelected = new EventEmitter<Recipe>();
 
   private recipes: Recipe[] = [
-    new Recipe('A Test Recipe', 'This is simply a test', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg'),
-    new Recipe('Another Test Recipe', 'This is simply a test', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg')
+    new Recipe(
+      'A Test Recipe',
+      'This is simply a test',
+      'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg',
+      [
+        new Ingredient('Meat',1),
+        new Ingredient('Corn',5)
+      ]),
+    new Recipe(
+      'Another Test Recipe',
+      'This is simply a test',
+      'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg',
+        [
+          new Ingredient('chicken',1),
+          new Ingredient('broccolli',4)
+        ])
   ];
 
   getRecipe(){
