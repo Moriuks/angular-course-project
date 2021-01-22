8fc218127ab1bf658f92e2a8731b2723a27b22f7
Luis Mora <luismora371@gmail.com>
2021-01-22 07:55:00 -0600

    passing ingridients from recipes to shopping list practice

--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { ShoppingListService } from './../../shopping-list/shopping-list.service';
+import { Component, OnInit, Input, ViewChild, ElementRef } from '@angular/core';
 
 import { Recipe } from '../recipe.model';
+import { Ingredient } from 'src/app/shared/ingredient.model';
 
 @Component({
   selector: 'app-recipe-detail',
@@ -10,9 +12,19 @@ import { Recipe } from '../recipe.model';
 export class RecipeDetailComponent implements OnInit {
   @Input() recipe: Recipe;
 
-  constructor() { }
+  constructor(private slService: ShoppingListService) { }
 
   ngOnInit() {
+  }
+
+  onAddItem(){
+    const ingredients = this.recipe.ingredients;
+    console.log('--------')
+    console.log(ingredients);
+    for (let ingredient of ingredients){
+      console.log(ingredient);
+      this.slService.addIngredient(ingredient)
+    }
   }
 
 }
